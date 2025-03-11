2a5ef09b2e43952dfcf1d51cb8ad91dfbf27188b
TahirAthirShah <supercellidtahir@gmail.com>
2025-03-12 00:14:52 +0530

    hiii

--- src/main/java/com/gitExample/GitExampleApplication.java
+++ src/main/java/com/gitExample/GitExampleApplication.java
@@ -8,6 +8,9 @@ public class GitExampleApplication {
 
 	public static void main(String[] args) {
 		int x=100;
+                   int c;
+                   int e;
+
 		SpringApplication.run(GitExampleApplication.class, args);
 	}
 
